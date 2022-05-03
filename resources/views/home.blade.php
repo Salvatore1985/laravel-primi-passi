b8e9f49e258e58f0e9b517874e7a0662a37dd6b9
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-05-03 15:54:35 +0200

    Hello World,

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel-primi-passi</title>
+</head>
+<body>
+    {{-- <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div> --}}
+    <h1>Hello Word</h1>
+</body>
+</html>
